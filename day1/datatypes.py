cc4834394f3e7fa843347d21ecd9db7296658032
Emmanuel Tweneboah <emmanuel.tweneboah@turntabl.io>
2022-10-26 13:16:45 +0000

    will continue day1 later

---
+++ day1/datatypes.py
@@ -0,0 +1,5 @@
+# Write a script that prompts the user to enter a number.
+#  Read this number and print a message depending on whether it is odd or even.
+
+number = input('Enter any number: ')
+print(number)
--- day1/introduction.py
+++ day1/introduction.py
@@ -12,12 +12,10 @@ if number%2 ==1:
 elif number%2 == 0:
     print(f"{number} is an even number")
 
-######### 
+#########
 print(f"I am {age} years old")
 print(f"My name is {name}")
 print(f"My name is {name} and I am {age} years old")
-
-
 
 random.random()              # selects a value between 0.0 and 1.0
 random.randint(100,200)      # integers between 100 and 200
@@ -44,7 +42,44 @@ else:
 mylist =  [1, 4, 9, 16, 25]
 
 for x in mylist:
-    print(x)
+    if x%2==0:
+         print(x)
 
+   
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
+
+current_age = 23
+first_age = 1
+
+thisList = []
+
+while first_age <= current_age:
+    thisList.append(first_age);
+    print(first_age)
+    first_age += 1
+print(thisList)
+print(sum(thisList))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
