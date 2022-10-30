c90b62fb29fb0703c9db7547b23cc5b040129ee7
Emmanuel Tweneboah <emmanuel.tweneboah@turntabl.io>
2022-10-30 16:49:15 +0000

    better understanding of python functiond but dont really get the recursion

---
+++ day1/functions_recap.py
@@ -0,0 +1,31 @@
+def add(a,b) : print(a+b)
+print(add(7,8))
+print(type(add))
+
+#
+
+#my_function("Emil", "Tobias", "Linus")
+
+def my_function1(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function1(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(5)
